4440fb197a46231b8bc22eb82463a0dddde0f1ae
Suprame4 <adriancortesgonz@gmail.com>
2023-03-06 17:19:39 -0800

    Complete Get musicians part 2

--- server.js
+++ server.js
@@ -5,7 +5,17 @@ const {sequelize} = require("./db")
 
 const port = 3000;
 
+app.use(express.json())
 //TODO
+app.get('/musicians', async (req, res) => {
+    const allMusicians = await Musician.findAll()    
+    res.json(allMusicians)
+})
+
+app.get('/musicians/:id', async (req, res) => {
+    const artist = await Musician.findByPk(req.params.id)
+    res.json(artist)
+})
 
 app.listen(port, () => {
     sequelize.sync();
