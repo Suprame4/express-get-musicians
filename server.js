e7b74f8ef836e7fd8b34d11699b00f6f5749ebcd
Suprame4 <adriancortesgonz@gmail.com>
2023-03-07 19:52:47 -0800

    Complete get musicians part 3 exercise

--- server.js
+++ server.js
@@ -17,6 +17,33 @@ app.get('/musicians/:id', async (req, res) => {
     res.json(artist)
 })
 
+//get musicians part 3 
+//create a new musician
+app.post('/musicians', async (req, res) => {
+    const newArtist = await Musician.create(req.body)
+    
+    const allMusicians = await Musician.findAll()
+    res.json(allMusicians)
+})
+//update a musician
+app.put('/musicians/:id', async (req, res) => {
+    const updateMusician = await Musician.findByPk(req.params.id)
+    await updateMusician.update(req.body)
+
+    const allMusicians = await Musician.findAll()
+    res.json(allMusicians)
+})
+
+app.delete('/musicians/:id', async (req, res) => {
+    const deleteMusician = await Musician.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    const allMusicians = await Musician.findAll()
+    res.json(allMusicians)
+})
+
 app.listen(port, () => {
     sequelize.sync();
     console.log(`Listening on port ${port}`)
